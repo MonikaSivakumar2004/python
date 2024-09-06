e63068666f3b9292728e75d02128de9a4e8f16df
MONIKA <monikasivakumar240@gmail.com>
2024-09-06 22:41:44 +0530

    Create Swap.py

---
+++ Swap.py
@@ -0,0 +1,9 @@
+def swapPositions(lis, pos1, pos2):
+    temp=lis[pos1]
+    lis[pos1]=lis[pos2]
+    lis[pos2]=temp
+    return lis
+List = [23, 65, 19, 90]
+pos1, pos2 = 1, 3
+
+print(swapPositions(List, pos1-1, pos2-1))
