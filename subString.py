950697a7824ad060a01ba2a82622d700ea0d4fd1
MONIKA <monikasivakumar240@gmail.com>
2024-09-06 22:33:42 +0530

    Create subString.py

---
+++ subString.py
@@ -0,0 +1,11 @@
+text = "Monika is an engineer!"
+
+if "monika" in text:
+    print("Substring found!")
+else:
+    print("Substring not found!")
+
+if "For" in text:
+    print("Substring found!")
+else:
+    print("Substring not found!")
